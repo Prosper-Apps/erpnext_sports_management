7ffb4c1155ebe58cdf63abd7d7d120d31373323b
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-09-23 13:24:59 +0000

    added single round robin matches

--- sports_management/sports_management/doctype/tournament/tournament.py
+++ sports_management/sports_management/doctype/tournament/tournament.py
@@ -3,6 +3,7 @@
 
 import frappe
 import datetime
+from itertools import cycle, islice
 from frappe.model.document import Document
 
 class Tournament(Document):
@@ -20,63 +21,69 @@ def create_matches(tournament):
 	starting_day = tournament_doc.starting_day
 	teams = tournament_doc.teams
 
-	# Calculate the number of rounds based on the number of teams
-	num_teams = len(teams)
-	num_rounds = num_teams - 1 if num_teams % 2 == 0 else num_teams
-
 	# Create a list of team names
 	team_names = [team.team for team in teams]
 
 	# Create a list of game days
-	game_days = []
-	current_day = starting_day
-	for i in range(num_rounds):
-		game_days.append(current_day)
-		current_day += datetime.timedelta(days=game_day_interval)
-
-	# Create a list of matches for each round
-	matches = []
-	for i in range(num_rounds):
-		round_matches = []
-		for j in range(num_teams // 2):
-			home = team_names[j]
-			away = team_names[num_teams - j - 1]
-			round_matches.append((home, away))
-		matches.append(round_matches)
-
-		# Rotate the team names for the next round
-		team_names = [team_names[0]] + [team_names[-1]] + team_names[1:-1]
-
-	# Create Game Day and Match documents and save them
-	for game_day in game_days:
+	num_teams = len(team_names)
+	num_rounds = num_teams - 1 if num_teams % 2 == 0 else num_teams
+	game_days = [starting_day + datetime.timedelta(days=i*game_day_interval) for i in range(num_rounds)]
+	  
+	rounds = circle_method(num_teams)
+	  
+	# Create the matches
+	for r in range(len(rounds)):
 		game_day_doc = frappe.new_doc('Game Day')
 		game_day_doc.tournament = tournament
-		game_day_doc.start = game_day
+		game_day_doc.start = game_days[r]
 		game_day_doc.insert()
-
-		for round_matches in matches:
-			for home, away in round_matches:
-				match_doc = frappe.new_doc('Match')
-				match_doc.tournament = tournament
-				match_doc.game_day = game_day_doc.name
-				match_doc.home = home
-				match_doc.guest = away
-				match_doc.date = game_day
-				match_doc.time = time_for_games
-				match_doc.insert()
-
-		# For double round robin tournaments, create a second round of matches
-		if schedule_type == 'Double Round Robin':
-			round_matches = [(away, home) for home, away in round_matches]
-			for home, away in round_matches:
-				match_doc = frappe.new_doc('Match')
-				match_doc.tournament = tournament
-				match_doc.game_day = game_day_doc.name
-				match_doc.home = home
-				match_doc.guest = away
-				match_doc.date = game_day
-				match_doc.time = time_for_games
-				match_doc.insert()
+		for match in rounds[r]:
+			match_doc = frappe.new_doc('Match')
+			match_doc.tournament = tournament
+			match_doc.game_day = game_day_doc.name
+			match_doc.home = team_names[match[0]-1]
+			match_doc.guest = team_names[match[1]-1]
+			match_doc.date = game_days[r]
+			match_doc.time = time_for_games
+			match_doc.insert()
 
 	# Send a frappe message to the user
 	frappe.msgprint('Matches created successfully!')
+	
+def circle_method(teams):
+
+	# If there is an odd amount of teams,
+	# there will be 1 more 'non-existent' team, standing for no match-up
+	rounds = teams - 1
+	if teams % 2 == 1:
+		rounds = teams
+	# Matches per round
+	mpr = int((rounds+1) / 2)
+
+	# Table of teams [1, 2, ..., n]
+	t = []
+	for i in range(rounds+1):
+		t.append(i+1)
+
+	# Stores the rounds with the corresponding matches inside
+	# e.g.: [[(1, 4), (2, 3)], [(1, 3), (4, 2)], [(1, 2), (3, 4)]]
+	matches = []
+	for r in range(rounds):
+		matches.append([])
+		for m in range(mpr):
+			matches[r].append((t[m], t[-1-m]))
+		t.remove(rounds-r+1)
+		t.insert(1, rounds-r+1)
+
+	# If there is an odd amount of teams,
+	# there will be 1 more 'non-existent' team, standing for no match-up
+	# Remove this non-existent team from the matches
+	if teams % 2 == 1:
+		for r in range(len(matches)):
+			for m in range(len(matches[r])):
+				if matches[r][m][0] == teams + 1 or matches[r][m][1] == teams + 1:
+					del matches[r][m]
+					break
+
+	return matches
+	
