8503bca7f1945b9d52bd6b3aede05e063521c9aa
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-09-25 11:53:47 +0000

    moved tournament team to not child

--- sports_management/sports_management/doctype/tournament_team/tournament_team.py
+++ sports_management/sports_management/doctype/tournament_team/tournament_team.py
@@ -1,8 +1,19 @@
 # Copyright (c) 2023, KAINOTOMO PH LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class TournamentTeam(Document):
-	pass
+	def validate(self):
+		validate_unique_tournament_team(self, "save")
+
+
+def validate_unique_tournament_team(doc, method):
+	# Check if the tournament and team link fields are set
+	if doc.tournament and doc.team:
+		# Check if there is another "Tournament Team" document with the same tournament and team link fields
+		existing_doc = frappe.db.get_value("Tournament Team", {"tournament": doc.tournament, "team": doc.team})
+		if existing_doc and existing_doc != doc.name:
+			frappe.throw(_("A team can only participate in a tournament once"))
