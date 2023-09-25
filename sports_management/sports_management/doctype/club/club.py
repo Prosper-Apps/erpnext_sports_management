2a1cab967f575e39b9f077fad13586f322a4b7eb
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-09-25 04:04:11 +0000

    added club webviews

--- sports_management/sports_management/doctype/club/club.py
+++ sports_management/sports_management/doctype/club/club.py
@@ -1,8 +1,18 @@
 # Copyright (c) 2023, KAINOTOMO PH LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.model.document import Document
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
 
-class Club(Document):
-	pass
+class Club(WebsiteGenerator):
+	def get_context(self, context):
+		# Get the address doctype of the venue and add it to the context
+		context.address = frappe.get_doc("Address", self.address)
+
+		# Get the contact doctype of the venue and add it to the context
+		context.contact = frappe.get_doc("Contact", self.contact)
+
+		# Get the venue doctype and add it to the context
+		context.venue = frappe.get_doc("Venue", self.venue)
+
+
