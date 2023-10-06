0156885245d1f75aa947d3d80903c0011f13f093
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-10-06 12:09:29 +0000

    Add starting line and bench in team roster Fixes #16

---
+++ sports_management/sports_management/doctype/team_roster/team_roster.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2023, KAINOTOMO PH LTD and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Team Roster', {
+	refresh: function(frm) {
+		// Limit the position column to only show the coach position
+		frm.set_query("position", function() {
+			return {
+				"filters": [
+					["Position", "role", "=", "Player"]
+				]
+			};
+		}
+		);
+	}
+});
