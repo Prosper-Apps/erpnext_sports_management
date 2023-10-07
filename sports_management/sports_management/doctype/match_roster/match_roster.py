7bd0f05e503c1123a10b90b4654bfefc2688d7f8
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-10-07 13:20:22 +0000

    added match roster

---
+++ sports_management/sports_management/doctype/match_roster/match_roster.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2023, KAINOTOMO PH LTD and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.model.document import Document
+
+class MatchRoster(Document):
+	def before_insert(self):
+		if frappe.db.exists('Match Roster', {'team': self.team, 'person': self.person, 'role': self.role}):
+			frappe.throw(_('This person with same role is already part of this team.'))
