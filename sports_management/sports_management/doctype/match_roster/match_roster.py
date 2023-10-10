eecbe137619eef32d9f8892c8045abf9a2a62ea4
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-10-10 12:41:53 +0000

    Fix match roster creation Fixes #63

--- sports_management/sports_management/doctype/match_roster/match_roster.py
+++ sports_management/sports_management/doctype/match_roster/match_roster.py
@@ -7,5 +7,5 @@ from frappe.model.document import Document
 
 class MatchRoster(Document):
 	def before_insert(self):
-		if frappe.db.exists('Match Roster', {'team': self.team, 'person': self.person, 'role': self.role}):
-			frappe.throw(_('This person with same role is already part of this team.'))
+		if frappe.db.exists('Match Roster', {'match': self.match, 'team': self.team, 'person': self.person, 'role': self.role}):
+			frappe.throw(_('This person with same role is already part of this match.'))
