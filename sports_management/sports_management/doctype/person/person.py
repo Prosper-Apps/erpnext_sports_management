be94fcad975e83e21148cd36875e8a81305ebd09
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-10-11 05:45:25 +0000

    Add player statistics Fixes #69

--- sports_management/sports_management/doctype/person/person.py
+++ sports_management/sports_management/doctype/person/person.py
@@ -6,15 +6,14 @@ from frappe.website.website_generator import WebsiteGenerator
 
 class Person(WebsiteGenerator):
 	def get_context(self, context):
-		# Get the address doctype and add it to the context
-		context.address = frappe.get_doc("Address", self.address)
-
-		# Get the contact doctype and add it to the context
-		context.contact = frappe.get_doc("Contact", self.contact)
 
 		# Get a list of the teams that the person is a member of
-		context.teams = frappe.get_all("Team Player", filters={"person": self.name}, fields=["parent"])
+		context.team_rosters = frappe.get_all("Team Roster", filters={"person": self.name}, fields=["team", "position", "role", "shirt_number"])
 
 		# For each team get the team name and route
-		for team in context.teams:
-			team.route = frappe.get_value("Team", team.parent, "route")
+		for team_roster in context.team_rosters:
+			team_roster.team_route = frappe.get_value("Team", team_roster.team, "route")
+			team_roster.team_name = frappe.get_value("Team", team_roster.team, "team_name")
+			team_roster.team_picture = frappe.get_value("Team", team_roster.team, "picture")
+			team_roster.position_name = frappe.get_value("Position", team_roster.position, "position_name")
+			team_roster.position_route = frappe.get_value("Position", team_roster.position, "route")
