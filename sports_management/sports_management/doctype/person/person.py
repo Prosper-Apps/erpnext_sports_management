ce6df96f65672849fd5840be35633545b33de5a2
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-10-11 12:08:10 +0000

    Add player statistics Fixes #69

--- sports_management/sports_management/doctype/person/person.py
+++ sports_management/sports_management/doctype/person/person.py
@@ -34,6 +34,8 @@ class Person(WebsiteGenerator):
 				match_roster.stats = {}
 				# Sum the goals scored by the person
 				match_roster.stats['goals'] = len([event for event in match_roster.match_events if event.events_type == "Goal"])
+				# Sub the own goals scored by the person
+				match_roster.stats['own_goals'] = len([event for event in match_roster.match_events if event.events_type == "Own Goal"])
 				# Sum the yellow cards received by the person
 				match_roster.stats['yellow_cards'] = len([event for event in match_roster.match_events if event.events_type == "Yellow Card"])
 				# Sum the red cards received by the person
@@ -57,6 +59,21 @@ class Person(WebsiteGenerator):
 				match_roster.stats['substitute_in'] = substitute_in
 				match_roster.stats['substitute_out'] = substitute_out
 				match_roster.stats['minutes_played'] = substitute_out - substitute_in
+
+			# Calculate tournament stats
+			tournament.stats = {}
+			# Sum the appearances of the person
+			tournament.stats['appearances'] = len(tournament.match_rosters)
+			# Sum the goals scored by the person
+			tournament.stats['goals'] = sum([match_roster.stats['goals'] for match_roster in tournament.match_rosters])
+			# Sum the own goals scored by the person
+			tournament.stats['own_goals'] = sum([match_roster.stats['own_goals'] for match_roster in tournament.match_rosters])
+			# Sum the yellow cards received by the person
+			tournament.stats['yellow_cards'] = sum([match_roster.stats['yellow_cards'] for match_roster in tournament.match_rosters])
+			# Sum the red cards received by the person
+			tournament.stats['red_cards'] = sum([match_roster.stats['red_cards'] for match_roster in tournament.match_rosters])
+			# Sum the minutes played by the person
+			tournament.stats['minutes_played'] = sum([match_roster.stats['minutes_played'] for match_roster in tournament.match_rosters])
 
 			# Order tournament.match_rosters by match date
 			tournament.match_rosters.sort(key=lambda x: x.match.date, reverse=False)
