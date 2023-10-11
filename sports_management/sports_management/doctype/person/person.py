2f5d09a15692301f439ffa6e301e4debe41edf2d
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-10-11 11:55:55 +0000

    Add player statistics Fixes #69

--- sports_management/sports_management/doctype/person/person.py
+++ sports_management/sports_management/doctype/person/person.py
@@ -17,3 +17,44 @@ class Person(WebsiteGenerator):
 			team_roster.team_picture = frappe.get_value("Team", team_roster.team, "picture")
 			team_roster.position_name = frappe.get_value("Position", team_roster.position, "position_name")
 			team_roster.position_route = frappe.get_value("Position", team_roster.position, "route")
+
+		# Get a list of all tournaments that are published
+		context.tournaments = frappe.get_all("Tournament", filters={"published": 1}, fields=["route", "tournament_name", "picture", "regular_play_time"])
+		for tournament in context.tournaments:
+
+			# Get a list of all match rosters for the tournament that the person is a member of
+			tournament.match_rosters = frappe.get_all("Match Roster", filters={"tournament": tournament.name, "person": self.name}, fields=["name", "tournament", "match", "team", "position", "role", "shirt_number", "starting_lineup"])	
+
+			for match_roster in tournament.match_rosters:
+				match_roster.match = frappe.get_doc("Match", match_roster.match)
+				# Get a list of all match events for the match that the person is a member of
+				match_roster.match_events = frappe.get_all("Match Event", filters={"match": match_roster.match.name, "match_roster": match_roster.name}, fields=["events_type", "minute"])
+				
+				# Calculate match_roster stats
+				match_roster.stats = {}
+				# Sum the goals scored by the person
+				match_roster.stats['goals'] = len([event for event in match_roster.match_events if event.events_type == "Goal"])
+				# Sum the yellow cards received by the person
+				match_roster.stats['yellow_cards'] = len([event for event in match_roster.match_events if event.events_type == "Yellow Card"])
+				# Sum the red cards received by the person
+				match_roster.stats['red_cards'] = len([event for event in match_roster.match_events if event.events_type == "Red Card"])
+				# Get the substitute in minute from the match_roster.match_events
+				substitute_in = [event.minute for event in match_roster.match_events if event.events_type == "Substitute In"]
+				# if substitute_in is not empty then assign as zero
+				if substitute_in:
+					substitute_in = substitute_in[0]
+				elif match_roster.starting_lineup == 0:
+					substitute_in = tournament.regular_play_time
+				else:
+					substitute_in = 0
+				# Get the substiture out minute from the match_roster.match_events
+				substitute_out = [event.minute for event in match_roster.match_events if event.events_type == "Substitute Out"]
+				if substitute_out:
+					substitute_out = substitute_out[0]
+				else:
+					substitute_out = tournament.regular_play_time
+
+				match_roster.stats['minutes_played'] = substitute_out - substitute_in
+
+			# Order tournament.match_rosters by match date
+			tournament.match_rosters.sort(key=lambda x: x.match.date, reverse=False)
