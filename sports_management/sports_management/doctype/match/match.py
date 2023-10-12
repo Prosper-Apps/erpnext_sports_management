2bb5489169bda5bc653144b372d2be23bcd17a75
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-10-12 12:23:40 +0000

    Format match view Fixes #82

--- sports_management/sports_management/doctype/match/match.py
+++ sports_management/sports_management/doctype/match/match.py
@@ -8,16 +8,23 @@ from frappe.website.website_generator import WebsiteGenerator
 class Match(WebsiteGenerator):
 
 	def get_context(self, context):
-		# Get the venue, tournament, game day, home team and guest team aling with their name and route
-		# Add all the data to the context to replace the existing
-		if self.venue:
-			context.venue = frappe.get_doc('Venue', self.venue)
-		if self.tournament:
-			context.tournament = frappe.get_doc('Tournament', self.tournament)
-		if self.game_day:
-			context.game_day = frappe.get_doc('Game Day', self.game_day)
+
+		# Set the page title
+		context.page_title = self.name
+
+		# Set creadrumbs
+		context.parents = [{'name': 'Home', 'route': '/'},  {'name': 'Matches', 'route': '/matches'}]
+
+		context.tournament = frappe.get_doc('Tournament', self.tournament)
+		context.game_day = frappe.get_doc('Game Day', self.game_day)
 		context.home = frappe.get_doc('Team', self.home)
+		context.home_club = frappe.get_doc('Club', context.home.club)
 		context.guest = frappe.get_doc('Team', self.guest)
+		context.guest_club = frappe.get_doc('Club', context.guest.club)
+
+		# Get the match rosters
+		context.home_rosters = frappe.get_list('Match Roster', filters={'match': self.name, 'team': self.home, 'role': 'Player'}, fields=['person', 'person.route as person_route', 'person_name', 'role', 'position', 'position.position_name', 'shirt_number', 'starting_lineup'], order_by='starting_lineup desc')
+		context.guest_rosters = frappe.get_list('Match Roster', filters={'match': self.name, 'team': self.guest, 'role': 'Player'}, fields=['person', 'person.route as person_route', 'person_name', 'role', 'position', 'position.position_name', 'shirt_number', 'starting_lineup'], order_by='starting_lineup desc')
 
 	# create function when saving a match to calculate the points for each team
 	def on_update(self):
