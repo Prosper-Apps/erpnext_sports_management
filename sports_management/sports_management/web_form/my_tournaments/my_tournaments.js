178f7ad1ccde04819a538cac3930c87c6dff61f2
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-12-13 12:13:41 +0000

    Update version number and modify web form for my tournaments

--- sports_management/sports_management/web_form/my_tournaments/my_tournaments.js
+++ sports_management/sports_management/web_form/my_tournaments/my_tournaments.js
@@ -1,3 +1,21 @@
 frappe.ready(function() {
-	// bind events here
-})
+
+	frappe.call({
+		method: 'sports_management.sports_management.web_form.my_tournaments.my_tournaments.get_website_item_route',
+		args: {
+			"team_tournament_name": frappe.web_form.doc.name
+		},
+		callback: function(response) {
+			var website_item_route = response.message;
+
+			// if website_item_route is not set, then return
+			if (!website_item_route) return;
+
+			// Add a button to the page that links to the website_item_route
+			$('<a class="btn btn-primary btn-sm ml-2" href="/' + website_item_route + '">PAY NOW</a>').appendTo('.web-form-actions');
+
+			// Add a message to the page indicating that product is not paid
+			$('<div class="alert alert-warning" role="alert">This registration has not been accepted yet. If you did not pay please do.<br/>If you already paid and more than 5 work days passed contact with us.</div>').appendTo('.section-body');
+		}
+	});
+});
--- sports_management/sports_management/web_form/my_tournaments/my_tournaments.py
+++ sports_management/sports_management/web_form/my_tournaments/my_tournaments.py
@@ -1,5 +1,42 @@
 import frappe
 
 def get_context(context):
-	# do your magic here
+	# Get the current user
+	current_user = frappe.session.user
+
+	# Get all team_tournaments where the current user is the owner
+	team_tournaments = frappe.get_all('Team Tournament', filters={'owner': current_user}, fields=['*'])
+
+	# Get the Rankinkgs for each team_tournament
+	for team_tournament in team_tournaments:
+		rankings = frappe.get_all('Ranking', filters={'team': team_tournament['team']}, fields=['*'])
+		# Get the first ranking
+		if rankings:
+			ranking = rankings[0]
+			# If the ranking disabled get the tournament name
+			if ranking['disabled']:
+				tournament = frappe.get_doc('Tournament', ranking['tournament'])
+				# Get the website item route
+				context.website_item_route = frappe.get_doc('Website Item', tournament.website_item).route
+		else:
+			context.website_item_route = None
+
 	pass
+
+@frappe.whitelist(allow_guest=False)
+def get_website_item_route(team_tournament_name):
+	website_item_route = None
+
+	# Get the tournament name
+	team_tournament = frappe.get_doc('Team Tournament', team_tournament_name)
+	rankings = frappe.get_all('Ranking', filters={'team': team_tournament.team, "tournament": team_tournament.tournament, "disabled": 1}, fields=['*'])
+	# Get the first ranking
+	if rankings:
+		ranking = rankings[0]
+		# If the ranking disabled get the tournament name
+		tournament = frappe.get_doc('Tournament', ranking['tournament'])
+		# Get the website item route
+		website_item_route = frappe.get_doc('Website Item', tournament.website_item).route
+
+	return website_item_route
+		
